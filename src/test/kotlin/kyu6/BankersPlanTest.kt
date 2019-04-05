3811bc69de0af8ead26ec4cc3b450a1d1286bfab
Paul Shields <pkshields@gmail.com>
2019-04-05 21:38:43 +0100

    Reset the test for Banker's Plan back to the default as provided by the Codewars website.

--- src/test/kotlin/kyu6/BankersPlanTest.kt
+++ src/test/kotlin/kyu6/BankersPlanTest.kt
@@ -7,10 +7,8 @@ class BankersPlanTest {
     @Test
     fun test1() {
         println("Fixed Tests: fortune")
-//        testing(fortune(100000, 1.0, 2000, 15, 1.0), true)
+        testing(fortune(100000, 1.0, 2000, 15, 1.0), true)
         testing(fortune(100000, 1.0, 9185, 12, 1.0), false)
-
-//        testing(fortune(2410562, 3.0, 160995, 13, 6.0), true)
     }
 
     companion object {
