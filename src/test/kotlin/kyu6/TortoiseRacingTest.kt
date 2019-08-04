79008bee782da64b39f1962a4f7e079c2808b7cb
Paul Shields <pkshields@gmail.com>
2019-08-04 23:37:45 +0100

    Fixed failing test.

--- src/test/kotlin/kyu6/TortoiseRacingTest.kt
+++ src/test/kotlin/kyu6/TortoiseRacingTest.kt
@@ -10,6 +10,6 @@ class TortoiseRacingTest {
         assertArrayEquals(intArrayOf(0, 32, 18), race(720, 850, 70))
         assertArrayEquals(intArrayOf(3, 21, 49), race(80, 91, 37))
 
-        assertArrayEquals(intArrayOf(18, 20, 59), race(820, 850, 550))
+        assertArrayEquals(intArrayOf(18, 20, 0), race(820, 850, 550))
     }
 }
