d746bc8670dab18635789cc769d9dee8656d956b
Paul Shields <pkshields@gmail.com>
2019-11-03 22:46:52 +0000

    Added solution for the 6 kyu "Invoke a String" kata.

---
+++ src/main/kotlin/kyu6/trickykotlin/InvokeAString.kt
@@ -0,0 +1,3 @@
+package kyu6.trickykotlin
+
+operator fun <T> String.invoke(obj: T) = obj
---
+++ src/test/kotlin/kyu6/trickykotlin/InvokeAStringTest.kt
@@ -0,0 +1,32 @@
+package kyu6.trickykotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.util.*
+
+class InvokeAStringTest {
+    @Test
+    fun testString() {
+        assertEquals("wow, I am the argument!", "this is a string, invoke it!"("wow, I am the argument!"))
+        assertEquals("s", "this is a string, invoke it!"("s"))
+    }
+
+    @Test
+    fun testInt() {
+        val r = Random(System.currentTimeMillis())
+        (0..100).forEach { r.nextInt().let { assertEquals(it, "this is another string"(it)) } }
+    }
+
+    @Test
+    fun testLong() {
+        val r = Random(System.currentTimeMillis())
+        (0..100).forEach { r.nextLong().let { assertEquals(it, "oh come on!"(it)) } }
+    }
+
+    @Test
+    fun testFile() {
+        val r = Random(System.currentTimeMillis())
+        (0..100).forEach { r.nextLong().let { assertEquals(File(it.toString()), "oh come on!"(File(it.toString()))) } }
+    }
+}
